382a7f5e68c47bc01251f2aa74edc3b8b92e8d52
LeTranThaiNguu <thainguu3007@gmail.com>
2024-05-28 09:10:22 +0700

    Buoi3

---
+++ resources/views/components/product-new.blade.php
@@ -0,0 +1,54 @@
+<section id="product-new" class="p-3">
+    <h2>Sản phẩm mới nhất</h2>
+    <div class="row">
+        <div class="col-sm-3">
+            <a href="chi-tiet-san-pham/1">
+                <x-product-card/>
+            </a>
+        </div>
+        <div class="col-sm-3">
+            <a href="chi-tiet-san-pham/1">
+                <x-product-card/>
+            </a>
+        </div>
+        <div class="col-sm-3">
+            <a href="chi-tiet-san-pham/1">
+                <div class="card">
+                    <img src="{{ asset('images/sanpham1.jpg') }}" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-3">
+            <a href="chi-tiet-san-pham/1">
+                <x-product-card/>
+        </div>
+        </a>
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+            <a href="chi-tiet-san-pham/1">
+                <x-product-card/>
+            </a>
+        </div>
+        <div class="col-sm-3">
+            <a href="chi-tiet-san-pham/1">
+                <x-product-card/>
+            </a>
+        </div>
+        <div class="col-sm-3">
+            <a href="chi-tiet-san-pham/1">
+                <x-product-card/>
+            </a>
+        </div>
+        <div class="col-sm-3">
+            <a href="chi-tiet-san-pham/1">
+                <x-product-card/>
+            </a>
+        </div>
+    </div>
+</section>
